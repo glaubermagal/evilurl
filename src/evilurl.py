0b2457239176d7033eb02d03d50d6934495a03e1
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-11-08 19:17:14 -0300

    improved output

--- src/evilurl.py
+++ src/evilurl.py
@@ -1,12 +1,18 @@
+import pandas as pd
+import argparse
 from pathlib import Path
 import socket
 import sys
 from itertools import product
 from urllib.parse import urlsplit
 import json
+from tabulate import tabulate
+from publicsuffix2 import get_sld, get_tld
 
-header = """
- ██████████ █████   █████ █████ █████          █████  █████ ███████████   █████
+
+TABLE_HEADERS=["homograph_domain", "punycode", "dns", "mixed", "combinations"]
+HEADER = """
+\033[31m ██████████ █████   █████ █████ █████          █████  █████ ███████████   █████
 ░░███░░░░░█░░███   ░░███ ░░███ ░░███          ░░███  ░░███ ░░███░░░░░███ ░░███
  ░███  █ ░  ░███    ░███  ░███  ░███           ░███   ░███  ░███    ░███  ░███
  ░██████    ░███    ░███  ░███  ░███           ░███   ░███  ░██████████   ░███
@@ -15,50 +21,76 @@ header = """
  ██████████    ░░███      █████ ███████████    ░░████████   █████   █████ ███████████
 ░░░░░░░░░░      ░░░      ░░░░░ ░░░░░░░░░░░      ░░░░░░░░   ░░░░░   ░░░░░ ░░░░░░░░░░░
 
-[ by @glaubermagal – https://github.com/glaubermagal/evilurl]
+[ by @glaubermagal – https://github.com/glaubermagal/evilurl]\033[0m
 """
 
 class HomographAnalyzer:
-    def __init__(self, unicode_combinations, show_domains_only, show_mixed_only, show_registered_only):
+    def __init__(self, unicode_combinations, show_domains_only, show_mixed_only, show_registered_only, json_format):
         self.unicode_combinations = unicode_combinations
         self.show_domains_only = show_domains_only
         self.show_mixed_only = show_mixed_only
+        self.json_format = json_format
         self.show_registered_only = show_registered_only
+        self.character_descriptions = {}
+        
+        for item in self.unicode_combinations:
+            for similar_dict in item['similar']:
+                for script, char_map in similar_dict.items():
+                    self.character_descriptions.update(char_map)
+
+    @staticmethod
+    def colored_text(text, color_code):
+        return f"\033[{color_code}m{text}\033[0m"
 
     def is_mixed_domain(self, punycode_encoded_domain, families):
         return not (punycode_encoded_domain.count("-") == 2 and len(families) == 1)
     
     def check_domain_registration(self, domain_name):
         try:
-            dns = socket.gethostbyname(domain_name)
-            return dns if dns else None
-        except socket.error as e:
+            return socket.gethostbyname(domain_name)
+        except socket.error:
             return None
 
+    
     def extract_domain_parts(self, url):
-        parsed_url = urlsplit('https://' + url)
-        domain_parts = parsed_url.netloc.split('.')
-        return domain_parts
+        # Get the full TLD (including SLD + ccTLD)
+        tld = get_tld(url)
+        
+        # Get the second-level domain (SLD)
+        sld = get_sld(url)
+        
+        # Find the position where the TLD starts
+        idx = url.find(tld)
+        
+        # Extract the subdomain part (if any)
+        subdomain = url[:idx].strip('.')
+        
+        # Combine subdomain and SLD for 'blog.example'
+        domain_without_tld = url[:idx].strip('.')
+        
+        # Return a dictionary with components
+        return {
+            'subdomain': subdomain,
+            'sld': sld,
+            'tld': tld,
+            'full_domain': url,
+            'domain_without_tld': domain_without_tld
+        }
 
     def generate_combinations(self, domain_parts):
         result = []
-        chars = set()
-        families = set()
+        chars, families = set(), set()
 
         for part in domain_parts:
             variations = [part]
             for char in part:
                 similar_chars_entry = next(
                     (entry['similar'][0] for entry in self.unicode_combinations if entry['latin'] == char), None)
-
-                if not self.show_domains_only:
-                    print('similar_chars_entry', similar_chars_entry)
-                if similar_chars_entry is not None:
+                if similar_chars_entry:
                     family, characters = list(similar_chars_entry.items())[0]
                     chars.update(characters)
                     variations.extend(characters)
                     families.add(family)
-
             result.append(variations)
 
         return result, list(chars), list(families)
@@ -66,65 +98,60 @@ class HomographAnalyzer:
     def analyze_domain(self, domain):
         domain = domain.lower()
         domain_parts = self.extract_domain_parts(domain)
-        result = self.generate_combinations(domain_parts[0])
-        combinations = result[0]
-        chars = result[1]
-        families = result[2]
+        combinations, chars, families = self.generate_combinations(domain_parts.get("domain_without_tld"))
 
-        unique_domains = set()
-
-        for combination in product(*combinations):
-            new_domain = ''.join(combination) + '.' + '.'.join(domain_parts[1:])
-            unique_domains.add(new_domain)
-
+        # unique_domains = {''.join(comb) + '.' + '.'.join(domain_parts[1:]) for comb in product(*combinations)}
+        unique_domains = {''.join(comb) for comb in product(*combinations)}
         if len(unique_domains) <= 1:
-            return print(f"No unicode combinations found for the current character set")
+            return print(self.colored_text("No unicode combinations found for the current character set", 31))
 
         if not self.show_domains_only:
-            print(header)
-            print(f"\033[32m[\033[0m*\033[32m]\033[0m Domain: \033[33m{domain}\033[0m")
-            print(f"\033[32m[\033[0m*\033[32m]\033[0m Homograph characters used: \033[32m{chars}\033[0m")
+            print(HEADER)
+            print(f"{self.colored_text('[*]', 32)} Domain: {self.colored_text(domain, 33)}")
+            print(f"{self.colored_text('[*]', 32)} Homograph characters used: {self.colored_text(chars, 32)}")
 
-        if self.show_domains_only:
-            for new_domain in unique_domains:
-                punycode_encoded_domain = new_domain.encode('idna').decode()
+        # Table data collection
+        table_data = []
+        for index, main_domain_part in enumerate(unique_domains):
+            full_domain = main_domain_part + '.' + domain_parts.get("tld")
+            formatted_combinations = []
+            punycode_encoded_domain = main_domain_part.encode('idna').decode()
+            if main_domain_part == punycode_encoded_domain:
+                continue
 
-                dns = self.check_domain_registration(new_domain)
-                if not dns and self.show_registered_only:
-                    continue
-                    
-                if self.show_mixed_only and not self.is_mixed_domain(punycode_encoded_domain, families):
-                    continue
-                
-                if new_domain == punycode_encoded_domain:
-                    continue
+            dns = self.check_domain_registration(full_domain)
+            if (not dns and self.show_registered_only) or (self.show_mixed_only and not self.is_mixed_domain(punycode_encoded_domain, families)):
+                continue
 
-                print(new_domain)
-        else:
-            for index, new_domain in enumerate(unique_domains):
-                punycode_encoded_domain = new_domain.encode('idna').decode()
-                is_mixed_domain = self.is_mixed_domain(punycode_encoded_domain, families)
-                
-                dns = self.check_domain_registration(new_domain)
-                if not dns and self.show_registered_only:
-                    continue
+            for part in main_domain_part:
+                for char in part:
+                    if char in self.character_descriptions.keys():
+                        formatted_combinations.append(f"{char} → {self.character_descriptions.get(char)}")
 
-                if self.show_mixed_only and not is_mixed_domain:
-                    continue
-                
-                if domain == punycode_encoded_domain:
-                    continue
+            if self.show_domains_only:
+                print(full_domain)
+                continue
 
-                print(f"\n{index + 1} -------------------------------")
-                print(f"Homograph Domain: {new_domain}")
-                print(f"Punycode: {punycode_encoded_domain}")
+            # Join formatted combinations into a single string
+            combinations_str = "\n".join(formatted_combinations)
+            table_data.append([
+                full_domain,
+                punycode_encoded_domain,
+                dns if dns else 'UNSET',
+                "YES" if self.is_mixed_domain(punycode_encoded_domain, families) else "NO",
+                combinations_str
+            ])
 
-                if dns:
-                    print(f"\033[31mDNS: {dns}\033[0m")
-                else:
-                    print(f"DNS: UNSET")
-                
-                print('\033[33mMixed: NO\033[0m' if not is_mixed_domain else 'Mixed: YES')
+        if self.json_format:
+            # Use pandas to structure JSON output
+            df = pd.DataFrame(table_data, columns=TABLE_HEADERS)
+            json_output = df.to_dict(orient="records")
+            print(json.dumps(json_output, indent=4))
+            return
+
+        if not self.show_domains_only:
+            print(tabulate(table_data, TABLE_HEADERS, tablefmt="grid"))
+
 
 def load_unicode_combinations_from_file(file_path):
     try:
@@ -135,40 +162,43 @@ def load_unicode_combinations_from_file(file_path):
         return None
 
 def main():
+    # Define argument parser
+    parser = argparse.ArgumentParser(description="Homograph Domain Analyzer")
+    parser.add_argument("domain", nargs="?", help="Domain to analyze")
+    parser.add_argument("-f", "--file", help="File path with domains to analyze")
+    parser.add_argument("--domains-only", action="store_true", help="Show domains only")
+    parser.add_argument("--mixed-only", action="store_true", help="Show mixed domains only")
+    parser.add_argument("--json", action="store_true", help="Shows the output as a JSON object")
+    parser.add_argument("--log-full", action="store_true", help="Log all information, including unregistered domains")
+
+    args = parser.parse_args()
+
+    # Load unicode combinations
     unicode_combinations_file = Path(__file__).resolve().parent / "./unicode_combinations.json"
     unicode_combinations = load_unicode_combinations_from_file(unicode_combinations_file)
 
-    if unicode_combinations is not None:
-        show_domains_only = False
-        show_mixed_only = False
-        show_registered_only = True
+    if unicode_combinations:
+        show_domains_only = args.domains_only
+        show_mixed_only = args.mixed_only
+        json_format = not not args.json
+        show_registered_only = not args.log_full
 
-        if '--domains-only' in sys.argv:
-            show_domains_only = True
-            sys.argv.remove('--domains-only')
+        homograph_analyzer = HomographAnalyzer(
+            unicode_combinations, show_domains_only, show_mixed_only, show_registered_only, json_format
+        )
 
-        if '--mixed-only' in sys.argv:
-            show_mixed_only = True
-            sys.argv.remove('--mixed-only')
-
-        if '--log-full' in sys.argv:
-            show_registered_only = False
-            sys.argv.remove('--log-full')
-
-        homograph_analyzer = HomographAnalyzer(unicode_combinations, show_domains_only, show_mixed_only, show_registered_only)
-
-        if len(sys.argv) == 2:
-            homograph_analyzer.analyze_domain(sys.argv[1])
-        elif len(sys.argv) == 3 and sys.argv[1] == '-f':
+        # Analyze a single domain or domains from a file
+        if args.domain:
+            homograph_analyzer.analyze_domain(args.domain)
+        elif args.file:
             try:
-                with open(sys.argv[2], 'r') as file:
-                    domains = file.read().splitlines()
-                    for domain in domains:
+                with open(args.file, 'r') as file:
+                    for domain in file.read().splitlines():
                         homograph_analyzer.analyze_domain(domain)
             except FileNotFoundError:
-                print(f"Error: File {sys.argv[2]} not found.")
+                print(f"Error: File {args.file} not found.")
         else:
-            print("Usage: python evilurl.py <domain> OR python evilurl.py -f <file_path>")
+            parser.print_help()
 
 if __name__ == "__main__":
     main()
