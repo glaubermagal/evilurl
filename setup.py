298b6f8aa9726cc19bd400a5cbe5ee8c12b30098
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-11-18 22:49:37 -0300

    fix missing dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='evilurl',
-    version='2.0.4',
+    version='2.0.5',
     packages=['src'],
     package_data={'src': ['unicode_combinations.json']},
     setup_requires=['wheel'],
